7044200838c36281b6aa85c0be807af21895887d
dane <838652739@qq.com>
2020-07-09 13:06:39 +0800

    311 1

--- ch2/comma/comma_310.go
+++ ch2/comma/comma_310.go
@@ -17,15 +17,36 @@ func main() {
 func comma(s string) string {
 	var buf bytes.Buffer
 
-	l := len(s)
+	b := []byte(s)
+
+	var start int
+	if b[0] == '+' || b[1] == '-' {
+		start = 1
+	} else {
+		start = 0
+	}
+
+	buf.Write(b[:start])
+
+	dot := bytes.Index(b, []byte{'.'})
+	if dot == -1 {
+		commaCore(b[start:], &buf)
+	} else {
+		commaCore(b[start:dot], &buf)
+		buf.Write(b[dot:])
+	}
+
+	return buf.String()
+}
+
+func commaCore(b []byte, buf *bytes.Buffer) {
+	l := len(b)
 
 	for start, i := 0, (l-1)%3; i < l; start, i = i+1, i+3 {
 		if start != 0 {
 			buf.WriteByte(',')
 		}
 
-		buf.WriteString(s[start : i+1])
+		buf.Write(b[start : i+1])
 	}
-
-	return buf.String()
 }
