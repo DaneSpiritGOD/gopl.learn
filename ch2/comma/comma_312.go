4b562d9c60e8fc8e7791a586220f14cee85a146a
dane <838652739@qq.com>
2020-07-15 12:55:53 +0800

    312

--- ch2/comma/comma_312.go
+++ ch2/comma/comma_312.go
@@ -11,10 +11,30 @@ func main() {
 		fmt.Println("please type two strings")
 		return
 	}
-	var a = 'c'
 	fmt.Println(compare(args[1], args[2]))
 }
 
 func compare(a, b string) bool {
-	return false
+
+	mapA := buildMap(a)
+	mapB := buildMap(b)
+
+	if len(mapA) != len(mapB) {
+		return false
+	}
+
+	for k, v := range mapA {
+		if v != mapB[k] {
+			return false
+		}
+	}
+	return true
+}
+
+func buildMap(s string) map[rune]int {
+	m := make(map[rune]int)
+	for _, c := range s {
+		m[c]++
+	}
+	return m
 }
