6f7020e7f01be4b827b6b739eb82188c27e30ae4
dane <838652739@qq.com>
2020-11-10 13:00:23 +0800

    ex5.12

---
+++ ch5/ex5.12/go.mod
@@ -0,0 +1,5 @@
+module github.com/DaneSpiritGOD/ch5/ex5.12
+
+go 1.15
+
+require golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
---
+++ ch5/ex5.12/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := findLinks("https://code.visualstudio.com/docs/editor/debugging")
+	if err != nil {
+		log.Printf("findlinks: %s", err)
+	}
+
+	var depth int
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			if elementNodeNoPair(n) {
+				fmt.Printf("%*s<%s/>\n", depth*2, "", n.Data)
+				return
+			}
+
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+			return
+		}
+	}
+
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			if elementNodeNoPair(n) {
+				return
+			}
+
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+			return
+		}
+	}
+
+	forEachNode(doc, startElement, endElement)
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+func hasElementNodeChild(n *html.Node) bool {
+	return n.FirstChild != nil && n.FirstChild.Type != html.ElementNode
+}
+
+func hasNoChildElementNode(n *html.Node) bool {
+	return n.FirstChild == nil
+}
+
+func elementNodeNoPair(n *html.Node) bool {
+	return hasElementNodeChild(n) || hasNoChildElementNode(n)
+}
+
+func findLinks(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return doc, nil
+}
