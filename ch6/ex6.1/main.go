a427c1b5e645881266ba7115344dced225ec09f2
dane <838652739@qq.com>
2020-11-24 12:32:52 +0800

    ex6.1 complete

--- ch6/ex6.1/main.go
+++ ch6/ex6.1/main.go
@@ -11,13 +11,25 @@ func main() {
 	x.Add(1)
 	x.Add(144)
 	x.Add(9)
-	fmt.Println(x.String()) // "{1 9 144}"
+	fmt.Printf("x: %s len: %d\n", x.String(), x.Len()) // "{1 9 144}"
+
+	xCopy := x.Copy()
+	fmt.Printf("xCopy: %s len: %d\n", xCopy.String(), xCopy.Len()) // "{1 9 144}"
+
+	xCopy.Remove(1000)
+	fmt.Printf("xCopy after remove 1000: %s len: %d\n", xCopy.String(), xCopy.Len()) // "{1 9 144}"
+
+	xCopy.Remove(1)
+	fmt.Printf("xCopy after remove 1: %s len: %d\n", xCopy.String(), xCopy.Len()) // "{1 9 144}"
+
+	xCopy.Clear()
+	fmt.Printf("xCopy after clear: %s len: %d\n", xCopy.String(), xCopy.Len()) // "{1 9 144}"
 
 	y.Add(9)
 	y.Add(42)
-	fmt.Println(y.String()) // "{9 42}"
+	fmt.Printf("y: %s len: %d\n", y.String(), y.Len()) // "{1 9 144}"
 
 	x.UnionWith(&y)
-	fmt.Println(x.String())           // "{1 9 42 144}"
-	fmt.Println(x.Has(9), x.Has(123)) // "true false"
+	fmt.Printf("x after union with y: %s len: %d\n", x.String(), x.Len()) // "{1 9 42 144}"
+	fmt.Println(x.Has(9), x.Has(123))                                     // "true false"
 }
