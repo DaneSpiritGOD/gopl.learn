99724fa5da93f971ccd985161ff0a865a2bd4a07
dane <838652739@qq.com>
2020-12-15 13:05:51 +0800

    ex7.11 step1

--- ch7/ex7.11/main.go
+++ ch7/ex7.11/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 )
-
 
 type dollars float32
 
@@ -15,6 +16,47 @@ type database map[string]dollars
 var db = database{"shoes": 50, "socks": 5}
 
 func main() {
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
 
-	http.Handle()
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	item := query.Get("item")
+
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	priceS := query.Get("price")
+	price, err := strconv.ParseFloat(priceS, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "price is not a float value: %f\n", price)
+		return
+	}
+
+	db[item] = dollars(price)
+	fmt.Fprintf(w, "updated item %s successfully %f\n", item, price)
 }
