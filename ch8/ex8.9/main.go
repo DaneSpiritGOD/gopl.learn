f1555320729e21eb0105d26f7d905002712ebb82
dane <838652739@qq.com>
2021-02-01 19:37:40 +0800

    ex8.9 completed

--- ch8/ex8.9/main.go
+++ ch8/ex8.9/main.go
@@ -6,21 +6,60 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 var directoryF = flag.String("d", ".", "the directory to monitor")
-var periodF = flag.Int("p", 5, "period of showing size of directory")
+var periodF = flag.Int("p", 5, "period(seconds) of showing size of directory")
 
+// go run main.go -d "D:\360极速浏览器下载" -p 3
 func main() {
 	flag.Parse()
 
 	dir := *directoryF
 	period := *periodF
+	log.Printf("dir: %s period: %ds\n", dir, period)
 
-	log.Printf("dir: %s, period: %d\n", dir, period)
+	tick := time.Tick(time.Duration(period) * time.Second)
+	for {
+		select {
+		case <-tick:
+			fileSizes := make(chan int64)
+
+			wg := &sync.WaitGroup{}
+			wg.Add(1)
+			go func() {
+				walkDir(dir, fileSizes, wg)
+			}()
+
+			go func() {
+				wg.Wait()
+				close(fileSizes)
+			}()
+
+			var nbytes int64
+			var nfiles int64
+			for size := range fileSizes {
+				nfiles++
+				nbytes += size
+			}
+
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
 }
 
+func printDiskUsage(nfiles, nbytes int64) {
+	log.Printf("| %d files  %.3f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+var sema = make(chan struct{}, 20)
+
 func getDirEntries(dir string) []os.FileInfo {
+	sema <- struct{}{}
+	defer func() { <-sema }()
+
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Printf("read dir error: %v\n", err)
@@ -28,12 +67,18 @@ func getDirEntries(dir string) []os.FileInfo {
 	return entries
 }
 
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, fileSizes chan<- int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	entries := getDirEntries(dir)
 	for _, e := range entries {
 		if e.IsDir() {
+			wg.Add(1)
 			subDir := filepath.Join(dir, e.Name())
-			walkDir(subDir, fileSizes)
+
+			go func() {
+				walkDir(subDir, fileSizes, wg)
+			}()
 		} else {
 			fileSizes <- e.Size()
 		}
