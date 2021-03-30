eb096f526b5c37667ffefad63e6a36d9db36ee58
dane.dai <Dane.dai@wisetechglobal.com>
2021-03-30 19:23:05 +0800

    ex11.2 move files to avoid package warning

---
+++ ch11/ex11.2/intsets/intset_test.go
@@ -0,0 +1,12 @@
+package intsets_test
+
+import (
+	"testing"
+
+	"github.com/DaneSpiritGOD/ex11.2/intsets"
+)
+
+func TestAdd(t *testing.T) {
+	var x intsets.IntSet
+	x.Add(1)
+}
--- ch11/ex11.2/intsets/intsets.go
+++ ch11/ex11.2/intsets/intsets.go
@@ -1,4 +1,4 @@
-package intset
+package intsets
 
 import (
 	"bytes"
