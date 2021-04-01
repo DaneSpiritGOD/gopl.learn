194a3dd0d7214a3ba99ce8abb397838be661b27b
dane.dai <Dane.dai@wisetechglobal.com>
2021-04-01 12:17:16 +0800

    ex12.2 init

---
+++ ch12/ex12.1/go.mod
@@ -0,0 +1,3 @@
+module github.com/DaneSpiritGOD/ex12.1
+
+go 1.16
---
+++ ch12/ex12.1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "github.com/DaneSpiritGOD/ex12.1/display"
+
+type movie struct {
+	Title, Subtitle string
+	Year            int
+	Color           bool
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+func main() {
+	strangelove := movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Color:    false,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	display.Display("movie", strangelove)
+}
